c092317b9fc0bcf9b711e4981e5d36f8aa1f8ba1
DALLA-COSTA <DL332594>
2020-09-16 09:50:19 +0200

    base de données complétée

--- Design/bdd.sql
+++ Design/bdd.sql
@@ -32,6 +32,19 @@ CREATE TABLE motif_de_frais(
 	,CONSTRAINT motif_de_frais_PK PRIMARY KEY (id_mdf)
 )ENGINE=InnoDB;
 
+--
+-- Déchargement des données de la table `motif_de_frais`
+--
+
+INSERT INTO `motif_de_frais` (`id_mdf`, `lib_mdf`) VALUES
+(1, 'Réunion'),
+(2, 'Compétition régionale'),
+(3, 'Compétition nationale'),
+(4, 'Compétition internationnale'),
+(5, 'Stage');
+
+-- --------------------------------------------------------
+
 
 #------------------------------------------------------------
 # Table: type_utilisateur
@@ -42,6 +55,15 @@ CREATE TABLE type_utilisateur(
         lib_type_util Varchar (50) NOT NULL
 	,CONSTRAINT type_utilisateur_PK PRIMARY KEY (id_type_util)
 )ENGINE=InnoDB;
+
+--
+-- Déchargement des données de la table `type_utilisateur`
+--
+
+INSERT INTO `type_utilisateur` (`id_type_util`, `lib_type_util`) VALUES
+(1, 'administrateur'),
+(2, 'contrôleur'),
+(3, 'adhérent');
 
 
 #------------------------------------------------------------
@@ -61,6 +83,13 @@ CREATE TABLE utilisateur(
 	,CONSTRAINT utilisateur_type_utilisateur_FK FOREIGN KEY (id_type_util) REFERENCES type_utilisateur(id_type_util)
 )ENGINE=InnoDB;
 
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`email_util`, `password_util`, `nom_util`, `prenom_util`, `statut_util`, `matricule_cont`, `id_type_util`) VALUES
+('dclucas31@gmail.com', 'mdp115', 'DC', 'Lucas', '0', '123', 1),
+('ligue@ligue.fr', 'ligue', 'ligue', 'ligue', '0', '0000000', 1);
 
 #------------------------------------------------------------
 # Table: ligue
@@ -78,6 +107,20 @@ CREATE TABLE ligue(
 	,CONSTRAINT ligue_utilisateur_FK FOREIGN KEY (email_util) REFERENCES utilisateur(email_util)
 )ENGINE=InnoDB;
 
+--
+-- Déchargement des données de la table `ligue`
+--
+
+INSERT INTO `ligue` (`id_ligue`, `lib_ligue`, `URL_ligue`, `contact_ligue`, `telephone_ligue`, `email_util`) VALUES
+(1, 'Natation', 'www.grandest.ffnatation.fr', 'www.grandest.ffnatation.fr', '03.83.18.87.32', 'ligue@ligue.fr'),
+(2, 'Tennis', 'www.ligue-grandest-fft.fr', 'www.ligue-grandest-fft.fr', '03 83 67 60 10', 'ligue@ligue.fr'),
+(3, 'Cyclisme', 'www.grandestcyclisme.fr', 'www.grandestcyclisme.fr', '09 61 06 27 79', 'ligue@ligue.fr'),
+(4, 'Equitation', 'www.cregrandest.fr', 'www.cregrandest.fr', '03.83.15.87.30', 'ligue@ligue.fr'),
+(5, 'Athlétisme', 'www.large.athle.fr', 'www.large.athle.fr', '03 26 06 75 83', 'ligue@ligue.fr'),
+(6, 'Judo', 'www.judograndest.fr', 'www.judograndest.fr', '03 88 26 94 11', 'ligue@ligue.fr');
+
+-- --------------------------------------------------------
+
 
 #------------------------------------------------------------
 # Table: club
@@ -94,6 +137,23 @@ CREATE TABLE club(
 
 	,CONSTRAINT club_ligue_FK FOREIGN KEY (id_ligue) REFERENCES ligue(id_ligue)
 )ENGINE=InnoDB;
+
+--
+-- Déchargement des données de la table `club`
+--
+
+INSERT INTO `club` (`id_club`, `lib_club`, `adr1_club`, `adr2_club`, `adr3_club`, `id_ligue`) VALUES
+(1, 'Dojo Burgien', '1 rue du Docteur DUBY', '1000', 'BOURG EN BRESSE', 6),
+(2, 'Saint Denis Dojo', '239 Allées des sports', '1000', 'ST DENIS LES BOURG', 6),
+(3, 'Judo Club Vallée Arbent', 'rue du Général ANDREA', '1100', 'ARBENT', 6),
+(4, 'Belli Judo', '1 rue du Bac', '1100', 'BELLIGNAT', 6),
+(5, 'Racing Club Montluel Judo', '170 rue des Chartinières', '1120', 'DAGNEUX', 6),
+(6, 'Centre Arts Martiaux Pondinois', 'rue de l Oiselon', '1160', 'PONT D AIN', 6),
+(7, 'Judo Club Ornex', '8 rue des Pralets', '1210', 'ORNEX', 6),
+(8, 'Dojo Gessien Valserine', 'Avenue des Voirons', '1220', 'DIVONNE LES BAINS', 6),
+(9, 'Dojo La Vallière', 'Complexe Sportif', '1250', 'MONTAGNAT', 6);
+
+-- --------------------------------------------------------
 
 
 #------------------------------------------------------------
