db17d330e87b709b3e81453b897803fbb4d61309
DALLA-COSTA <DL332594>
2020-09-23 10:34:57 +0200

    gestion

--- gestion/gestion_utilisateur.php
+++ gestion/gestion_utilisateur.php
@@ -11,7 +11,7 @@ $nb=0;
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="css/styles.css" type="text/css" />
+  <link rel="stylesheet" href="../css/styles.css" type="text/css" />
   <title>gestion</title>
 </head>
 
