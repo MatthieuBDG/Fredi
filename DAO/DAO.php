6a182e0295bdbc968081d39a00c358d71d90a742
Math_Bour <mathias.bourdim@limayrac.fr>
2020-09-23 08:56:35 +0200

    Création du DAO et init

---
+++ DAO/DAO.php
@@ -0,0 +1,24 @@
+<?php
+class DAO { 
+    protected $pdo=null; // Objet de connexion
+
+    /**
+     * Constructeur
+     */
+    public function __construct()
+    {
+        // On construit le DSN
+        $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
+        // Création de la connexion
+        try {
+            $pdo = new PDO($dsn, DB_USER, DB_PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo("<p>Erreur lors de la connexion : " . $e->getMessage().'<p>');
+        }
+        $this->pdo = $pdo;
+    } // function __construct
+
+    
+
+} // class DAO
