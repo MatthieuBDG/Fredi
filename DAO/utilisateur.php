8d2a2494e1ba9c1a343ecbb3d5229a6cc241b9b8
DALLA-COSTA <DL332594>
2020-10-06 09:17:08 +0200

    utilisateur

---
+++ DAO/utilisateur.php
@@ -0,0 +1,58 @@
+<?php
+class utilisateur {
+    private $email_util;
+    private $nom_util;
+    private $prenom_util;
+    private $id_type_util;
+
+ //constructeur
+    public function __construct(array $row) {
+        $this->email_util = $row['email_util'];
+        $this->nom_util = $row['nom_util'];
+        $this->prenom_util = $row['prenom_util'];
+        $this->id_type_util = $row['id_type_util'];
+    }
+
+    public function get_email_util(){
+		return $this->email_util;
+	}
+
+	public function set_email_util($email_util){
+		$this->email_util = $email_util;
+	}
+
+	public function get_nom_util(){
+		return $this->nom_util;
+	}
+
+	public function set_nom_util($nom_util){
+		$this->nom_util = $nom_util;
+	}
+
+	public function get_prenom_util(){
+		return $this->prenom_util;
+	}
+
+	public function set_prenom_util($prenom_util){
+		$this->prenom_util = $prenom_util;
+	}
+
+	public function get_id_type_util(){
+		return $this->id_type_util;
+	}
+
+	public function set_id_type_util($id_type_util){
+		$this->date_naissance_adh = $date_naissance_adh;
+  }
+  
+  public function lib_type_util($id_type_util){
+    if($id_type_util = 1) {
+      return 'administrateur';
+    } else if ($id_type_util = 2) {
+      return 'controleur';
+    } else {
+      return 'adhérent';
+    }
+  }
+
+}   
---
+++ DAO/utilisateurDAO.php
@@ -0,0 +1,54 @@
+<?php
+require_once('DAO.php');
+require_once('utilisateur.php');
+
+Class utilisateurDAO extends DAO {
+    
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function find($email_util) {
+        $sql = "SELECT * FROM utilisateur WHERE email_util= :email_util";
+        try {
+            $sth = $this->pdo->prepare($sql);
+            $sth->execute(array(
+                ':email_util' => $email_util
+            ));
+            $row = $sth->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            throw new Exception("Erreur lors de la requête SQL : " . $e->getMessage());
+        }
+
+        $utilisateur=null;
+        if ($row) {
+            $utilisateur = new utilisateur($row);
+        }
+        
+       
+        return $utilisateur;
+    }
+
+  
+    public function findAll() {
+        $sql = "SELECT * FROM u"; 
+
+        try {
+            $sth = $this->pdo->prepare($sql);
+            $sth->execute();
+            $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            throw new Exception("Erreur lors de la requête SQL : " . $e->getMessage());
+        }
+
+        $utilisateurs = array();
+
+        foreach ($rows as $row) {
+            $utilisateurs[] = new utilisateur($row);
+        }
+
+        return $utilisateurs;
+    }
+
+
+}
