8ba239fe518837a5c23f514b43cebb8bc09f7ae1
BOUBEE DE GRAMONT <BM325780>
2020-12-16 08:45:32 +0100

    update

--- gestion/modification_ligne_de_frais.php
+++ gestion/modification_ligne_de_frais.php
@@ -1,28 +1,50 @@
 <?php
 include '../connexion_dbh.php';
+include '../init.php';
+
+$dao = new Motif_fraisDAO();
+$motif_frais = $dao->findAll(); 
+
+$dao2 = new PeriodeDAO();
+$periode = $dao2->findAll();
+
+$dao3 = new adherentDAO();
+$adherent = $dao3->findAll();
+
 
 if(isset($_POST["back"])){
-    header('location: gestion_motif_de_frais'); 
+    header('location: gestion_ligne_de_frais'); 
 }
 if(isset($_GET["id_ldf"])){ 
     $id_ldf = $_GET["id_ldf"];
     if($_SESSION['id_type_util'] == 3){
-        $req_recup_info = $dbh->prepare("SELECT * FROM motif_de_frais WHERE id_ldf = ?");
+        $req_recup_info = $dbh->prepare("SELECT * FROM ligne_de_frais WHERE id_ldf = ?");
         $req_recup_info->execute(array($id_ldf));
         $resultat_req = $req_recup_info->fetch();
 
         if(isset($_POST["back"])){
-            header('location: gestion_motif_de_frais'); 
+            header('location: gestion_ligne_de_frais'); 
         }
     if(isset($_POST["submit"])){ // Debut de la inscription
-        $lib_mdf = htmlspecialchars($_POST['lib_mdf']);
-        $req_recup_lib_existe = $dbh->prepare("SELECT lib_mdf FROM motif_de_frais where lib_mdf = ?");
+        $lib_trajet_ldf = htmlspecialchars($_POST['lib_trajet_ldf']);
+        $date_ldf = htmlspecialchars($_POST['date_ldf']);
+        $cout_peage_ldf = htmlspecialchars($_POST['cout_peage_ldf']);
+        $cout_repas_ldf = htmlspecialchars($_POST['cout_repas_ldf']);
+        $cout_hebergement_ldf =  htmlspecialchars($_POST['cout_hebergement_ldf']);
+        $nb_km_ldf = htmlspecialchars($_POST['nb_km_ldf']);
+        $total_km_ldf = htmlspecialchars($_POST['total_km_ldf']);
+        $total_ldf = htmlspecialchars($_POST['total_ldf']);
+        $id_mdf = htmlspecialchars($_POST['id_mdf']);
+        $annee_per = htmlspecialchars($_POST['annee_per']);
+        $email_util = htmlspecialchars($_POST['email_util']);
+        $req_recup_lib_existe = $dbh->prepare("SELECT lib_mdf FROM ligne_de_frais where lib_mdf = ?");
         $req_recup_lib_existe->execute(array($lib_mdf));
         $resultat_lib = $req_recup_lib_existe->fetch();
+        
     if($resultat_lib == 0){
     if(!empty($lib_mdf)){
         
-            $req_update = $dbh->prepare("UPDATE motif_de_frais SET lib_mdf = ? WHERE id_ldf = ? ");
+            $req_update = $dbh->prepare("UPDATE ligne_de_frais SET lib_mdf = ? WHERE id_ldf = ? ");
             $req_update->execute(array($lib_mdf,$id_ldf)); 
             $modifier = "<h5>La période $lib_mdf a été modifié dans FREDI</h5>";
     
@@ -41,7 +63,7 @@ if(isset($_GET["id_ldf"])){
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Modification de motif de frais</title>
+        <title>Modification de ligne de frais</title>
         <link rel="stylesheet" href="../css/styles.css" type="text/css" />
     </head>
     <header>
@@ -64,11 +86,42 @@ if(isset($_GET["id_ldf"])){
     <body class="connexion">
     <div class="connexion">
         <center>
-          <h1>Modification de motif de frais</h1>
+          <h1>Modification de ligne de frais</h1>
             <br>
             <form method="post"> 
-             <p>Motif<br><input type="text" name="lib_mdf" placeholder="lib_mdf" value="<?php echo $resultat_req['lib_mdf']; ?>"require/></p>
-            
+             <p>Date<br><input type="date" name="date_ldf" placeholder="date_ldf" value="<?php echo $resultat_req['date_ldf']; ?>"require/></p>
+             <p>Nom trajet<br><input type="text" name="lib_trajet_ldf" placeholder="lib_trajet_ldf" value="<?php echo $resultat_req['lib_trajet_ldf']; ?>"require/></p>
+             <p>Cout péage<br><input type="text" name="cout_peage_ldf" placeholder="cout_peage_ldf" value="<?php echo $resultat_req['cout_peage_ldf']; ?>"require/></p>
+             <p>Cout repas<br><input type="text" name="cout_repas_ldf" placeholder="cout_repas_ldf" value="<?php echo $resultat_req['cout_repas_ldf']; ?>"require/></p>
+             <p>Cout hebergement<br><input type="text" name="cout_hebergement_ldf" placeholder="cout_hebergement_ldf" value="<?php echo $resultat_req['cout_hebergement_ldf']; ?>"require/></p>
+             <p>Nombre de trajet (km)<br><input type="text" name="nb_km_ldf" placeholder="nb_km_ldf" value="<?php echo $resultat_req['nb_km_ldf']; ?>"require/></p>
+             <p>Total nombre de trajet (km)<br><input type="text" name="total_km_ldf" placeholder="total_km_ldf" value="<?php echo $resultat_req['total_km_ldf']; ?>"require/></p>
+             <p>Total<br><input type="text" name="total_ldf" placeholder="total_ldf" value="<?php echo $resultat_req['total_ldf']; ?>"require/></p>
+             <p>Motif de frais</p>
+            <select name="id_mdf">
+            <?php
+            foreach ($motif_frais as $motif_frais) {
+                echo '<option value='.$motif_frais->get_id_mdf().'>'.$motif_frais->get_lib_mdf().'</option>';
+                }
+            ?>
+         </select>
+         <p>Periode</p>
+            <select name="annee_per">
+            <?php
+            foreach ($periode as $periode) {
+                echo '<option value='.$periode->get_annee_per().'>'.$periode->get_annee_per().'</option>';
+                }
+            ?>
+         </select>
+         <p>Adherent</p>
+            <select name="email_util">
+            <?php
+            foreach ($adherent as $adherent) {
+                echo '<option value='.$adherent->get_email_util().'>'.$adherent->get_email_util().'</option>';
+                }
+            ?>
+         </select>
+
              <br>
              <?php
              if(isset($erreur))
